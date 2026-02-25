de9ecb77030ff957cc71c7c9d5cd1c0decd932c1
Stanislav Turchinskiy <s.turchinskiy@yandex.ru>
2026-02-25 17:44:39 +0300

    homework 1,2

---
+++ 1_tree/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+const (
+	shiftItem     = "├───"
+	shiftLastItem = "└───"
+	shiftTopLevel = "│"
+)
+
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func dirTree(out io.Writer, dir string, printFiles bool) error {
+
+	return printDir(out, dir, printFiles, "")
+}
+
+func printDir(out io.Writer, dir string, printFiles bool, shift string) error {
+	list, err := os.ReadDir(dir)
+	if err != nil {
+		return fmt.Errorf("error read dir %s, error : %w", dir, err)
+	}
+
+	selectionList := selectionFiles(printFiles, list)
+
+	err = printItems(out, dir, printFiles, shift, selectionList, err)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func selectionFiles(printFiles bool, list []os.DirEntry) []os.DirEntry {
+	if printFiles {
+		return list
+	}
+
+	newList := make([]os.DirEntry, 0)
+	for _, item := range list {
+		if item.IsDir() {
+			newList = append(newList, item)
+		}
+
+	}
+	return newList
+}
+
+func printItems(out io.Writer, dir string, printFiles bool, shift string, list []os.DirEntry, err error) error {
+	lastIndex := len(list) - 1
+	for i, item := range list {
+
+		fileFullName := filepath.Join(dir, item.Name())
+
+		var builder strings.Builder
+
+		builder.WriteString(shift)
+		if i == lastIndex {
+			builder.WriteString(shiftLastItem)
+		} else {
+			builder.WriteString(shiftItem)
+		}
+
+		builder.WriteString(item.Name())
+
+		err = addSizeInfoFirFile(item, &builder, fileFullName)
+		if err != nil {
+			return err
+		}
+
+		builder.WriteString("\n")
+		_, err = out.Write([]byte(builder.String()))
+
+		if item.IsDir() {
+			newShift := shift
+			if i != lastIndex {
+				newShift = newShift + shiftTopLevel
+			}
+			newShift = newShift + "\t"
+			
+			err = printDir(out, fileFullName, printFiles, newShift)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func addSizeInfoFirFile(item os.DirEntry, builder *strings.Builder, fileFullName string) error {
+
+	if item.IsDir() {
+		return nil
+	}
+
+	finfo, err := item.Info()
+	if err != nil {
+		return fmt.Errorf("error get file info %s, error : %w", fileFullName, err)
+	}
+
+	builder.WriteString(" (")
+	if finfo.Size() == 0 {
+		builder.WriteString("empty")
+	} else {
+		builder.WriteString(strconv.FormatInt(finfo.Size(), 10) + "b")
+	}
+	builder.WriteString(")")
+
+	return nil
+}
---
+++ 2_signer/signer.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+func ExecutePipeline(jobs ...job) {
+
+	numWorkers := len(jobs)
+
+	wg := sync.WaitGroup{}
+	wg.Add(numWorkers)
+
+	dataChs := make([]chan interface{}, numWorkers+1)
+
+	for i := 0; i < numWorkers+1; i++ {
+
+		dataChs[i] = make(chan interface{}, MaxInputDataLen)
+
+	}
+
+	for i, job := range jobs {
+		go runJob(job, dataChs[i], dataChs[i+1], &wg, i == 0)
+	}
+
+	wg.Wait()
+}
+
+func runJob(job job, in, out chan interface{}, wg *sync.WaitGroup, firstJob bool) {
+	defer wg.Done()
+	job(in, out)
+	//а как получить имена джоб?
+	//log.Println("job end", time.Now())
+	close(out)
+	if firstJob {
+		close(in)
+	}
+}
+
+func SingleHash(in, out chan any) {
+
+	wg := sync.WaitGroup{}
+
+	for untypedValue := range in {
+
+		data, err := stringFromUntypedValue(untypedValue)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		crc32First := make(chan string)
+		crc32Second := make(chan string)
+
+		go func() {
+			crc32First <- DataSignerCrc32(data)
+		}()
+
+		md5 := DataSignerMd5(data)
+		go func(md5 string) {
+			crc32Second <- DataSignerCrc32(md5)
+		}(md5)
+
+		wg.Add(1)
+		go func(crc32First, crc32Second chan string, wg *sync.WaitGroup) {
+			defer wg.Done()
+			result := <-crc32First + "~" + <-crc32Second
+			out <- result
+			close(crc32First)
+			close(crc32Second)
+		}(crc32First, crc32Second, &wg)
+	}
+
+	wg.Wait()
+}
+
+func MultiHash(in, out chan interface{}) {
+
+	hashNumbers := 6
+	wgForMultiHash := sync.WaitGroup{}
+
+	for untypedValue := range in {
+
+		data, err := stringFromUntypedValue(untypedValue)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results := make([]string, hashNumbers)
+
+		wg := sync.WaitGroup{}
+		wg.Add(hashNumbers)
+
+		for i := 0; i < hashNumbers; i++ {
+			go func(i int) {
+				defer wg.Done()
+				results[i] = DataSignerCrc32(strconv.Itoa(i) + data)
+			}(i)
+		}
+
+		wgForMultiHash.Add(1)
+		go func(out chan interface{}, wgForMultiHash *sync.WaitGroup) {
+			defer wgForMultiHash.Done()
+			wg.Wait()
+			var res string
+			for _, value := range results {
+				res += value
+			}
+			out <- res
+		}(out, &wgForMultiHash)
+	}
+	wgForMultiHash.Wait()
+}
+
+func CombineResults(in, out chan interface{}) {
+
+	var results []string
+	for value := range in {
+		results = append(results, value.(string))
+	}
+
+	sort.Strings(results)
+
+	res := strings.Join(results, "_")
+	out <- res
+}
+
+func stringFromUntypedValue(untypedValue any) (string, error) {
+
+	var data string
+
+	switch untypedValue.(type) {
+	case string:
+		data = untypedValue.(string)
+	case int:
+		data = strconv.Itoa(untypedValue.(int))
+	default:
+		err := fmt.Errorf("unclown type input channel %v", reflect.TypeOf(untypedValue))
+		return "", err
+	}
+
+	return data, nil
+}
+
+func main() {
+
+	inputData := []int{0, 1, 1, 2, 3, 5, 8}
+
+	hashSignJobs := []job{
+		job(func(in, out chan interface{}) {
+			for _, fibNum := range inputData {
+				out <- fibNum
+			}
+		}),
+		job(SingleHash),
+		job(MultiHash),
+		job(CombineResults),
+		job(func(in, out chan interface{}) {
+			dataRaw := <-in
+			_, ok := dataRaw.(string)
+			if !ok {
+				log.Fatal("cant convert result data to string")
+			}
+			//testResult = data
+		}),
+	}
+
+	start := time.Now()
+
+	ExecutePipeline(hashSignJobs...)
+
+	end := time.Since(start)
+	fmt.Println(end)
+
+}
